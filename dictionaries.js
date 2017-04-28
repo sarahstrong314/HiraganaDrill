e2df8a6dcd14f61f94887de5d678b0efd7d2d73c
Sarah Strong <sa967st@gmail.com>
2017-04-28 12:52:40 -0400

    Added Lesson 1

---
+++ dictionaries.js
@@ -0,0 +1,20 @@
+var hiraganaDict = {'あ':'a', 'い':'i', 'う':'u',' え':'e', 'お':'o', 'か':'ka', 'き':'ki', 'く':'ku', 'け':'ke', 'こ':'ko', 'が':'ga', 'ぎ':'gi', 'ぐ':'gu', 'げ':'ge', 'ご':'go', 'さ':'sa', 'し':'shi', 'す':'su', 'せ':'se', 'そ':'so', 'ざ':'za','じ':'ji', 'ず':'zu', 'ぜ':'ze', 'ぞ':'zo', 'た':'ta', 'ち':'chi', 'つ':'tsu', 'て':'te', 'と':'to', 'だ':'da', 'で':'de', 'ど':'do', 'な':'na', 'に':'ni', 'ぬ':'nu', 'ね':'ne', 'の':'no', 'は':'ha', 'ひ':'hi', 'ふ':'fu', 'へ':'he', 'ほ':'ho', 'ば':'ba', 'び':'bi', 'ぶ':'bu', 'べ':'be', 'ぼ':'bo', 'ぱ':'pa', 'ぴ':'pi', 'ぷ':'pu', 'ぺ':'pe', 'ぽ':'po', 'ま':'ma', 'み':'mi', 'む':'mu', 'め':'me', "も":'mo', 'や':'ya', 'ゆ':'yu', 'よ':'yo', 'ら':'ra', 'り':'ri', 'る':'ru', 'れ':'re', 'ろ':'ro', 'わ':'wa', 'を':'wo', 'ん':'n'};
+var katakanaDict = {'ア':'a', 'イ':'i', 'ウ':'u', 'エ':'e', 'オ':'o', 'カ':'ka', 'キ':'ki', 'ク':'ku', 'ケ':'ke', 'コ':'ko', 'ガ':'ga', 'ギ':'gi', 'グ':'gu', 'ゲ':'ge', 'ゴ':'go', 'サ':'sa', 'シ':'shi', 'ス':'su', 'セ':'se', 'ソ':'so', 'ザ':'za','ジ':'ji', 'ズ':'zu', 'ゼ':'ze', 'ゾ':'zo', 'タ':'ta', 'チ':'chi', 'ツ':'tsu', 'テ':'te', 'ト':'to', 'ダ':'da', 'デ':'de', 'ド':'do', 'ナ':'na', 'ニ':'ni', 'ヌ':'nu', 'ネ':'ne', 'ノ':'no', 'ハ':'ha', 'ヒ':'hi', 'フ':'fu', 'ヘ':'he', 'ホ':'ho', 'バ':'ba', 'ビ':'bi', 'ブ':'bu', 'ベ':'be', 'ボ':'bo', 'パ':'pa', 'ピ':'pi', 'プ':'pu', 'ペ':'pe', 'ポ':'po', 'マ':'ma', 'ミ':'mi', 'ム':'mu', 'メ':'me', "モ":'mo', 'ヤ':'ya', 'ユ':'yu', 'ヨ':'yo', 'ラ':'ra', 'リ':'ri', 'ル':'ru', 'レ':'re', 'ロ':'ro', 'ワ':'wa', 'ヲ':'wo', 'ン':'n'};
+var dictOne = {'hon': 'book', 'kami': 'paper', 'enpitsu': 'pencil', 'kore': 'this one', 'sore': 'that one', 'are': 'that one (over there)', 'nan': 'what', 'watashi': 'I', 'anata': 'you', 'kare': 'he', 'kanojo': 'she', 'dare': 'who', 'nansai':'how old'}
+
+var hiragana = [];
+var katakana = [];
+var lessonOne = []
+var item;
+
+
+function fillArray(dict, array) {
+  for (item in dict) {
+  	if (array.indexOf(item) == -1) {
+      array.push(item);
+    }
+  }
+}
+
+fillArray(hiraganaDict, hiragana);
+
--- drill.js
+++ drill.js
@@ -1,31 +1,3 @@
-var hiraganaDict = {'あ':'a', 'い':'i', 'う':'u',' え':'e', 'お':'o', 'か':'ka', 'き':'ki', 'く':'ku', 'け':'ke', 'こ':'ko', 'が':'ga', 'ぎ':'gi', 'ぐ':'gu', 'げ':'ge', 'ご':'go', 'さ':'sa', 'し':'shi', 'す':'su', 'せ':'se', 'そ':'so', 'ざ':'za','じ':'ji', 'ず':'zu', 'ぜ':'ze', 'ぞ':'zo', 'た':'ta', 'ち':'chi', 'つ':'tsu', 'て':'te', 'と':'to', 'だ':'da', 'で':'de', 'ど':'do', 'な':'na', 'に':'ni', 'ぬ':'nu', 'ね':'ne', 'の':'no', 'は':'ha', 'ひ':'hi', 'ふ':'fu', 'へ':'he', 'ほ':'ho', 'ば':'ba', 'び':'bi', 'ぶ':'bu', 'べ':'be', 'ぼ':'bo', 'ぱ':'pa', 'ぴ':'pi', 'ぷ':'pu', 'ぺ':'pe', 'ぽ':'po', 'ま':'ma', 'み':'mi', 'む':'mu', 'め':'me', "も":'mo', 'や':'ya', 'ゆ':'yu', 'よ':'yo', 'ら':'ra', 'り':'ri', 'る':'ru', 'れ':'re', 'ろ':'ro', 'わ':'wa', 'を':'wo', 'ん':'n'};
-var katakanaDict = {'ア':'a', 'イ':'i', 'ウ':'u', 'エ':'e', 'オ':'o', 'カ':'ka', 'キ':'ki', 'ク':'ku', 'ケ':'ke', 'コ':'ko', 'ガ':'ga', 'ギ':'gi', 'グ':'gu', 'ゲ':'ge', 'ゴ':'go', 'サ':'sa', 'シ':'shi', 'ス':'su', 'セ':'se', 'ソ':'so', 'ザ':'za','ジ':'ji', 'ズ':'zu', 'ゼ':'ze', 'ゾ':'zo', 'タ':'ta', 'チ':'chi', 'ツ':'tsu', 'テ':'te', 'ト':'to', 'ダ':'da', 'デ':'de', 'ド':'do', 'ナ':'na', 'ニ':'ni', 'ヌ':'nu', 'ネ':'ne', 'ノ':'no', 'ハ':'ha', 'ヒ':'hi', 'フ':'fu', 'ヘ':'he', 'ホ':'ho', 'バ':'ba', 'ビ':'bi', 'ブ':'bu', 'ベ':'be', 'ボ':'bo', 'パ':'pa', 'ピ':'pi', 'プ':'pu', 'ペ':'pe', 'ポ':'po', 'マ':'ma', 'ミ':'mi', 'ム':'mu', 'メ':'me', "モ":'mo', 'ヤ':'ya', 'ユ':'yu', 'ヨ':'yo', 'ラ':'ra', 'リ':'ri', 'ル':'ru', 'レ':'re', 'ロ':'ro', 'ワ':'wa', 'ヲ':'wo', 'ン':'n'};
-var dictOne = {'hon': 'book', 'kami': 'paper', 'enpitsu': 'pencil', 'kore': 'this one', 'sore': 'that one', 'are': 'that one (over there)', 'nan': 'what', 'watashi': 'I', 'anata': 'you', 'kare': 'he', 'kanojo': 'she', 'dare': 'who', 'nansai':'how old'}
-
-var japaneseOne = [];
-var englishOne = [];
-var word;
-
-var hiragana = [];
-var katakana = [];
-var solutions = [];
-var character;
-
-
-for (character in hiraganaDict) {
-  hiragana.push(character);
-  solutions.push(hiraganaDict[character]);
-}
-
-for (character in katakanaDict) {
-  katakana.push(character);
-}
-
-for (word in dictOne) {
-  japaneseOne.push(word);
-  englishOne.push(dictOne[character]);
-}
-
 var currentMode = 0;
 var currentScore = 0;
 var numQuestions = 0;
@@ -40,18 +12,21 @@ function setMode(int) {
     document.getElementById('Katakana').disabled = false;
     document.getElementById('LessonOne').disabled = false;
     document.getElementById('Instructions').innerHTML = 'Select the correct pronunciation of the character above.'
+    fillArray(hiraganaDict, hiragana);
 
   } else if (int == 1) {
     document.getElementById('Hiragana').disabled = false;
     document.getElementById('Katakana').disabled = true;
     document.getElementById('LessonOne').disabled = false;
     document.getElementById('Instructions').innerHTML = 'Select the correct pronunciation of the character above.'
+    fillArray(katakanaDict, katakana);
 
   } else {
     document.getElementById('Hiragana').disabled = false;
     document.getElementById('Katakana').disabled = false;
     document.getElementById('LessonOne').disabled = true;
     document.getElementById('Instructions').innerHTML = 'Select the correct translation of the word above.'
+    fillArray(dictOne, lessonOne);
   }
 
   document.getElementById('Next').disabled = false;
@@ -66,6 +41,8 @@ function setMode(int) {
   document.getElementById('OptionThree').innerHTML = ''
 
   pickCharacter();
+
+  resetScore();
 
 }
 
@@ -86,16 +63,19 @@ function pickCharacter() {
     characters = katakana;
   } else {
     dictionary = dictOne;
-    characters = japaneseOne;
+    characters = lessonOne;
   }
 
-  var n = Math.floor(Math.random() * Object.keys(dictionary).length);
-  
-  document.getElementById('CharacterSpace').innerHTML = characters[n]; 
-  
-  displayKana(characters[n]);
-
-  document.getElementById('AmIRight').innerHTML = ''
+  if (characters.length == 0) {
+    document.getElementById('Instructions').innerHTML = 'Done!'
+    fillArray(dictionary, characters);
+  } else {
+    var n = Math.floor(Math.random() * Object.keys(dictionary).length);  
+    document.getElementById('CharacterSpace').innerHTML = characters[n];
+    displayKana(characters[n]);
+    //characters.splice(n, 1);
+    document.getElementById('AmIRight').innerHTML = ''
+  }
 }
 
 function displayKana(character) {
@@ -109,7 +89,7 @@ function displayKana(character) {
     characters = katakana;
   } else {
     dictionary = dictOne;
-    characters = japaneseOne;
+    characters = lessonOne;
   }
 
   var dictionaryLength = Object.keys(dictionary).length
