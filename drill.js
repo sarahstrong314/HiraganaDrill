d4b82f95f1e6e9e7cc34a0b08a579a3508739f72
Sarah Strong <sa967st@gmail.com>
2017-05-05 10:54:11 -0400

    Improved Code Redundancy

--- drill.js
+++ drill.js
@@ -2,30 +2,30 @@
 
 
 function setView() {
-  var viewMode = document.getElementById('ViewMode').selectedIndex;
-  if (viewMode == 0) document.getElementById('CharacterSpace').innerHTML = toKana(document.getElementById('CharacterSpace').innerHTML);
-  else document.getElementById('CharacterSpace').innerHTML = toRomaji(document.getElementById('CharacterSpace').innerHTML);
+  var view = document.getElementById('ViewMenu').selectedIndex;
+  if (view == 0) document.getElementById('Question').innerHTML = toKana(document.getElementById('Question').innerHTML);
+  else document.getElementById('Question').innerHTML = toRomaji(document.getElementById('Question').innerHTML);
 }
 
-var mode = 0;
+var lesson = 0;
 var allDicts = [hiraganaDict, katakanaDict, extraKatakanaDict, dict1, dict2, dict3, dict4, dict5, dict6, dict7, dict8, dict9, dict10, dict11, dict12, dict13];
 
-function setMode() {
-  mode = document.getElementById('LessonMenu').selectedIndex;
-  numLeft = Object.keys(allDicts[mode]).length;
-  fillArray(allDicts[mode], questions);
-  fillArray(allDicts[mode], allQuestions);
+function setLesson() {
+  lesson = document.getElementById('LessonMenu').selectedIndex;
+  numLeft = Object.keys(allDicts[lesson]).length;
+  fillArray(allDicts[lesson], questions);
+  fillArray(allDicts[lesson], allQuestions);
 
-  if (mode < 3) {
-    document.getElementById('ViewMode').selectedIndex = 0;
+  if (lesson < 3) {
+    document.getElementById('ViewMenu').selectedIndex = 0;
     document.getElementById('Romaji').disabled = true;
     document.getElementById('Instructions').innerHTML = 'Select the correct pronunciation of the character above.';
-  } else if (mode < 8) {
-    document.getElementById('ViewMode').selectedIndex = 1;
+  } else if (lesson < 8) {
+    document.getElementById('ViewMenu').selectedIndex = 1;
     document.getElementById('Romaji').disabled = false;
     document.getElementById('Instructions').innerHTML = 'Select the correct translation of the word above.';
   } else {
-    document.getElementById('ViewMode').selectedIndex = 0;
+    document.getElementById('ViewMenu').selectedIndex = 0;
     document.getElementById('Romaji').disabled = false;
     document.getElementById('Instructions').innerHTML = 'Select the correct translation of the word above.';
   }
@@ -38,7 +38,7 @@ function setMode() {
 
   document.getElementById('AmIRight').innerHTML = '';
 
-  pickCharacter();
+  pickQuestion();
 
   score = 0;
   numQuestions = 0;
@@ -47,9 +47,9 @@ function setMode() {
 
 function disableOptions(bool) {
   var options = [
-    document.getElementById('OptionOne'),
-    document.getElementById('OptionTwo'),
-    document.getElementById('OptionThree')
+    document.getElementById('Option1'),
+    document.getElementById('Option2'),
+    document.getElementById('Option3')
   ];
 
   for (var i = 0; i < options.length; i++) {
@@ -59,66 +59,60 @@ function disableOptions(bool) {
 
 var lastOne = false;
 
-function pickCharacter() {
+function pickQuestion() {
   document.getElementById('Next').disabled = true;
   disableOptions(false);
-  document.getElementById('OptionOne').className = "answerbutton";
-  document.getElementById('OptionTwo').className = "answerbutton";
-  document.getElementById('OptionThree').className = "answerbutton";
+  document.getElementById('Option1').className = "answer";
+  document.getElementById('Option2').className = "answer";
+  document.getElementById('Option3').className = "answer";
 
-  var dictionary = allDicts[mode];
-  var characters = questions;
-
-  if (characters.length == 1) lastOne = true;
-  var n = Math.floor(Math.random() * characters.length);  
+  if (questions.length == 1) lastOne = true;
+  var n = Math.floor(Math.random() * questions.length);  
   
-  if (document.getElementById('ViewMode').selectedIndex == 0) document.getElementById('CharacterSpace').innerHTML = characters[n];
-  else document.getElementById('CharacterSpace').innerHTML = toRomaji(characters[n]);
-  displayKana(characters[n]);
-  characters.splice(n, 1);
+  if (document.getElementById('ViewMenu').selectedIndex == 0) document.getElementById('Question').innerHTML = questions[n];
+  else document.getElementById('Question').innerHTML = toRomaji(questions[n]);
+  displayAnswers(questions[n]);
+  questions.splice(n, 1);
   document.getElementById('AmIRight').innerHTML = '';
 }
 
-function displayKana(character) {
-  var dictionary = allDicts[mode];
-  var characters = allQuestions;
+function displayAnswers(answer) {
+  var dictionaryLength = Object.keys(allDicts[lesson]).length;
 
-  var dictionaryLength = Object.keys(dictionary).length;
+  // picks two different random indices that don't match answer's index
 
-  // picks two different random indices that don't match character's index
+  var randomAnswer = allQuestions[Math.floor(Math.random() * dictionaryLength)];
+  var randomAnswer2 = allQuestions[Math.floor(Math.random() * dictionaryLength)];
 
-  var randomCharacter = characters[Math.floor(Math.random() * dictionaryLength)];
-  var randomCharacter2 = characters[Math.floor(Math.random() * dictionaryLength)];
+  while (answer == randomAnswer)
+    randomAnswer = allQuestions[Math.floor(Math.random() * dictionaryLength)];
 
-  while (character == randomCharacter)
-    randomCharacter = characters[Math.floor(Math.random() * dictionaryLength)];
-
-  while (randomCharacter2 == character || randomCharacter2 == randomCharacter)
-    randomCharacter2 = characters[Math.floor(Math.random() * dictionaryLength)];
+  while (randomAnswer2 == answer || randomAnswer2 == randomAnswer)
+    randomAnswer2 = allQuestions[Math.floor(Math.random() * dictionaryLength)];
 
   // puts the answers in order based on index
 
-  var answers = [character, randomCharacter, randomCharacter2];
+  var answers = [answer, randomAnswer, randomAnswer2];
 
   answers.sort();
 
   // displays the answers, in order, on the option buttons
 
   var options = [
-    document.getElementById('OptionOne'),
-    document.getElementById('OptionTwo'),
-    document.getElementById('OptionThree')
+    document.getElementById('Option1'),
+    document.getElementById('Option2'),
+    document.getElementById('Option3')
   ];
 
   for (var i = 0; i < options.length; i++) {
-    options[i].innerHTML = dictionary[answers[i]];
+    options[i].innerHTML = allDicts[lesson][answers[i]];
   }
 
   // once an option is clicked, displays whether it is correct or incorrect
 
-  options[0].onclick = function() { checkAnswer(character, answers[0], 'OptionOne') };
-  options[1].onclick = function() { checkAnswer(character, answers[1], 'OptionTwo') };
-  options[2].onclick = function() { checkAnswer(character, answers[2], 'OptionThree') };
+  options[0].onclick = function() { checkAnswer(answer, answers[0], 'Option1') };
+  options[1].onclick = function() { checkAnswer(answer, answers[1], 'Option2') };
+  options[2].onclick = function() { checkAnswer(answer, answers[2], 'Option3') };
 }
 
 var score = 0;
@@ -158,6 +152,6 @@ function checkAnswer(correct, answer, option) {
 function restart() {
   score = 0;
   numQuestions = 0;  
-  setMode(mode);
+  setLesson(lesson);
   document.getElementById('Score').innerHTML = 'Score: 0 out of 0' + ' (' + numLeft.toString() +' Remaining)';
 }
