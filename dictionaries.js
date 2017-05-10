a7ae44556d4a0bf30e76e28608b77b9661635587
Sarah Strong <sa967st@gmail.com>
2017-05-10 13:53:23 -0400

    Updated Verb Dictionaries

--- dictionaries.js
+++ dictionaries.js
@@ -22,10 +22,10 @@ var dict17 = {'おします':'to push', 'いきます':'to go', 'はらいます
 var dict18 = {'うたいます':'to sing', 'かいます':'to buy', 'さわります':'to touch', 'きります':'to cut', 'おくります':'to put', 'あるきます':'to walk', 'とめます':'to stop or to park', 'うんてんします':'to drive','します':'to do'};
 var dict19 = {'おふろに はいります':'to take a bath', 'てがみを だします':'to send a letter', 'にもつを おくります':'to send a package', 'おかねを だします':'to withdraw some money', 'けんぶつします':'to go sightseeing', 'かいものに いきます':'to shop', 'しつれんします':'to be brokenhearted', 'スポーツを します':'to play sports', 'そうじを します':'to clean'};
 
-var dict14_te ={'てつだいます(to help)':['てつだって', 'help'], 'いいます(to say)':['いって', 'say'], 'たちます(to stand)':['たって', 'stand'], 'まちます(to wait)':['まって', 'wait'], 'すわります(to sit)':['すわって', 'sit'], 'はいります(to enter)':['はいって', 'enter'], 'とります(to take)':['とって', 'take'], 'よびます(to call)':['よんで', 'call'], 'しにます(to die)':['しんで', 'die'], 'ひきます(to play (string instruments or piano))':['ひいて', 'play (string instruments or piano)'], 'いそぎます(to hurry)':['いそいで', 'hurry'], 'ぬぎます(to take off)':['ぬいで', 'take off'], 'みせます(to show)':['みせて', 'show'], 'さんぽを します(to go for a walk)':['さんぽを して', 'go for a walk'], 'あめが ふります(to rain)':['あめが ふって', 'rain'], 'ふくを きます(to wear clothes)':['ふくを きて', 'wear clothes'], 'ぼうしを かぶります(to wear a hat)':['ぼうしを かぶって', 'wear a hat']};  
-var dict17_nai ={'おきます(to put)':['おかない', 'to not put'], 'つくります(to make)':['つくらない', 'to not make'], 'おします(to push)':['おさない', 'to not push'], 'ぬぎます(to take off)':['ぬがない', 'to not take off'], 'いきます(to go)':['いかない', 'to not go'], 'はらいます(to pay)':['はらわない', 'to not pay'], 'もっていきます(to bring)':['もっていかない', 'to not bring'], 'あいます(to meet)':['あわない', 'to not meet'], 'つかいます(to use)':['つかわない', 'to not use'], 'とります(to take)':['とらない', 'to not take'], 'なくします(to lose (something))':['なくさない', 'to not lose (something)'], 'かえります(to return or go home)':['かえらない', 'to not go home'], 'くすりを のみます(to take medicine)':['くすりを のまない', 'to not take medicine'], 'かきます(to write)':['かかない', 'to not write'], 'かえします(to return (something))':['かえさない', 'to not return (something)'], 'はいります(to enter)':['はいらない', 'to not enter'], 'よびます(to call)':['よばない', 'to not call'], 'つけます(to turn on)':['つけない', 'to not turn on'], 'はなします(to speak)':['はなさない', 'to not speak'], 'だします(to hand in)':['ださない', 'to not hand in'], 'いれます(to put in)':['いれない', 'to not put in'], 'わすれます(to forget)':['わすれない', 'to not forget'], 'くるまを とめます(to park a car)':['くるまを とめない', 'to not park a car'], 'みせます(to show)':['みせない', 'to not show'], 'おきます1(to wake up)':['おきない', 'to not wake up'], 'おぼえます(to remember)':['おぼえない', 'to not remember'], 'たべます(to eat)':['たべない', 'to not eat'], 'でかけます(to go out)':['でかけない', 'to not go out'], 'アルバイトを します(to do a part-time job)':['アルバイトを しない', 'to not do a part-time job'], 'しんぱいします(to worry)':['しんぱいしない', 'to not worry'], 'きます(to come)':['こない', 'to not come'], 'しゅっちょうします(to go on a business trip)':['しゅっちょうしない', 'to not go on a business trip']};
-var dict18_dict ={'うたいます(to sing)':['うたう', 'to sing'], 'はらいます(to pay)':['はらう', 'to pay'], 'かいます(to buy)':['かう', 'to buy'], 'つくります(to make)':['つくる', 'to make'], 'さわります(to touch)':['さわる', 'to touch'], 'きります(to cut)':['きる', 'to cut'], 'のります(to ride)':['のる', 'to ride'], 'かえります(to return or go home)':['かえる', 'to return or go home'], 'おくります(to put)':['おくる', 'to put'], 'とります(to take)':['とる', 'to take'], 'しります(to know)':['しる', 'to know'], 'あそびます(to play or to hang out)':['あそぶ', 'to play or to hang out'], 'ひきます(to play (string instruments or piano))':['ひく', 'to play string instruments or piano'], 'あるきます(to walk)':['あるく', 'to walk'], 'はを みがきます(to brush one\'s teeth)':['はを みがく', 'to brush one\'s teeth'], 'おしえます(to teach)':['おしえる', 'to teach'], 'ねます(to sleep or to go to bed)':['ねる', 'to sleep or to go to bed'], 'とめます(to stop or to park)':['とめる', 'to stop or to park'], 'たべます(to eat)':['たべる', 'to eat'], 'みます(to see)':['みる', 'to see'], 'かります(to borrow)':['かりる', 'to borrow'], 'うんてんします(to drive)':['うんてんする', 'to drive'], 'きます(to come)':['くる', 'to come'], 'します(to do)':['する', 'to do'], 'あります(to have or to exist (thing))':['ある', 'to have or to exist (things)'], 'います(to have or to exist (living creature))':['いる', 'to have or to exist (living creature)']};
-var dict19_ta ={'のります(to ride)':['のった', 'rode'], 'のみます(to drink)':['のんだ', 'drank'], 'よみます(to read)':['よんだ', 'read'], 'いきます(to go)':['いった', 'went'], 'あそびます(to play or hang out)':['あそんだ', 'played or hung out'], 'あいます(to meet)':['あった', 'met'], 'おふろに はいります(to take a bath)':['おふろに はいった', 'took a bath'], 'てがみを だします(to send a letter)':['てがみを だした', 'sent a letter'], 'にもつを おくります(to send a package)':['にもつを おくった', 'sent a package'], 'およぎます(to swim)':['およいだ', 'swam'], 'ならいます(to learn)':['ならった', 'learned'], 'おかねを だします(to withdraw some money)':['おかねを だした', 'withdrew some money'], 'けんぶつします(to go sightseeing)':['けんぶつした', 'went sightseeing'], 'かきます(to write)':['かいた', 'wrote'], 'うたいます(to sing)':['うたった', 'sang'], 'かいものに いきます(to shop)':['かいものに いきました', 'shopped'], 'たべます(to eat)':['たべた', 'ate'], 'みます(to see)':['みた', 'saw'], 'ねます(to sleep or go to bed)':['ねた', 'slept or went to bed'], 'おります(to get off)':['おりた', 'got off'], 'かります(to borrow )':['かりた', 'borrowed'], 'しつれんします(to be brokenhearted)':['しつれんした', 'was brokenhearted'], 'スポーツを します(to play sports)':['スポーツを した', 'played sports'], 'あらいます(to wash)':['あらった', 'washed'], 'そうじを します(to clean)':['そうじを した', 'cleaned']};
+var dict14_te ={'てつだいます (to help)':['てつだって', 'help'], 'いいます (to say)':['いって', 'say'], 'たちます (to stand)':['たって', 'stand'], 'まちます (to wait)':['まって', 'wait'], 'すわります (to sit)':['すわって', 'sit'], 'はいります (to enter)':['はいって', 'enter'], 'とります (to take)':['とって', 'take'], 'よびます (to call)':['よんで', 'call'], 'しにます (to die)':['しんで', 'die'], 'ひきます (to play (string instruments or piano))':['ひいて', 'play (string instruments or piano)'], 'いそぎます (to hurry)':['いそいで', 'hurry'], 'ぬぎます (to take off)':['ぬいで', 'take off'], 'みせます (to show)':['みせて', 'show'], 'さんぽを します (to go for a walk)':['さんぽを して', 'go for a walk'], 'あめが ふります (to rain)':['あめが ふって', 'rain'], 'ふくを きます (to wear clothes)':['ふくを きて', 'wear clothes'], 'ぼうしを かぶります (to wear a hat)':['ぼうしを かぶって', 'wear a hat']};  
+var dict17_nai ={'おきます (to put)':['おかない', 'to not put'], 'つくります (to make)':['つくらない', 'to not make'], 'おします (to push)':['おさない', 'to not push'], 'ぬぎます (to take off)':['ぬがない', 'to not take off'], 'いきます (to go)':['いかない', 'to not go'], 'はらいます (to pay)':['はらわない', 'to not pay'], 'もっていきます (to bring)':['もっていかない', 'to not bring'], 'あいます (to meet)':['あわない', 'to not meet'], 'つかいます (to use)':['つかわない', 'to not use'], 'とります (to take)':['とらない', 'to not take'], 'なくします (to lose (something))':['なくさない', 'to not lose (something)'], 'かえります (to return or go home)':['かえらない', 'to not go home'], 'くすりを のみます (to take medicine)':['くすりを のまない', 'to not take medicine'], 'かきます (to write)':['かかない', 'to not write'], 'かえします (to return (something))':['かえさない', 'to not return (something)'], 'はいります (to enter)':['はいらない', 'to not enter'], 'よびます (to call)':['よばない', 'to not call'], 'つけます (to turn on)':['つけない', 'to not turn on'], 'はなします (to speak)':['はなさない', 'to not speak'], 'だします (to hand in)':['ださない', 'to not hand in'], 'いれます (to put in)':['いれない', 'to not put in'], 'わすれます (to forget)':['わすれない', 'to not forget'], 'くるまを とめます (to park a car)':['くるまを とめない', 'to not park a car'], 'みせます (to show)':['みせない', 'to not show'], 'おきます1 (to wake up)':['おきない', 'to not wake up'], 'おぼえます (to remember)':['おぼえない', 'to not remember'], 'たべます (to eat)':['たべない', 'to not eat'], 'でかけます (to go out)':['でかけない', 'to not go out'], 'アルバイトを します (to do a part-time job)':['アルバイトを しない', 'to not do a part-time job'], 'しんぱいします (to worry)':['しんぱいしない', 'to not worry'], 'きます (to come)':['こない', 'to not come'], 'しゅっちょうします (to go on a business trip)':['しゅっちょうしない', 'to not go on a business trip']};
+var dict18_dict ={'うたいます (to sing)':['うたう', 'to sing'], 'はらいます (to pay)':['はらう', 'to pay'], 'かいます (to buy)':['かう', 'to buy'], 'つくります (to make)':['つくる', 'to make'], 'さわります (to touch)':['さわる', 'to touch'], 'きります (to cut)':['きる', 'to cut'], 'のります (to ride)':['のる', 'to ride'], 'かえります (to return or go home)':['かえる', 'to return or go home'], 'おくります (to put)':['おくる', 'to put'], 'とります (to take)':['とる', 'to take'], 'しります (to know)':['しる', 'to know'], 'あそびます (to play or to hang out)':['あそぶ', 'to play or to hang out'], 'ひきます (to play (string instruments or piano))':['ひく', 'to play string instruments or piano'], 'あるきます (to walk)':['あるく', 'to walk'], 'はを みがきます (to brush one\'s teeth)':['はを みがく', 'to brush one\'s teeth'], 'おしえます (to teach)':['おしえる', 'to teach'], 'ねます (to sleep or to go to bed)':['ねる', 'to sleep or to go to bed'], 'とめます (to stop or to park)':['とめる', 'to stop or to park'], 'たべます (to eat)':['たべる', 'to eat'], 'みます (to see)':['みる', 'to see'], 'かります (to borrow)':['かりる', 'to borrow'], 'うんてんします (to drive)':['うんてんする', 'to drive'], 'きます (to come)':['くる', 'to come'], 'します (to do)':['する', 'to do'], 'あります (to have or to exist (thing))':['ある', 'to have or to exist (things)'], 'います (to have or to exist (living creature))':['いる', 'to have or to exist (living creature)']};
+var dict19_ta ={'のります (to ride)':['のった', 'rode'], 'のみます (to drink)':['のんだ', 'drank'], 'よみます (to read)':['よんだ', 'read'], 'いきます (to go)':['いった', 'went'], 'あそびます (to play or hang out)':['あそんだ', 'played or hung out'], 'あいます (to meet)':['あった', 'met'], 'おふろに はいります (to take a bath)':['おふろに はいった', 'took a bath'], 'てがみを だします (to send a letter)':['てがみを だした', 'sent a letter'], 'にもつを おくります (to send a package)':['にもつを おくった', 'sent a package'], 'およぎます (to swim)':['およいだ', 'swam'], 'ならいます (to learn)':['ならった', 'learned'], 'おかねを だします (to withdraw some money)':['おかねを だした', 'withdrew some money'], 'けんぶつします (to go sightseeing)':['けんぶつした', 'went sightseeing'], 'かきます (to write)':['かいた', 'wrote'], 'うたいます (to sing)':['うたった', 'sang'], 'かいものに いきます (to shop)':['かいものに いきました', 'shopped'], 'たべます (to eat)':['たべた', 'ate'], 'みます (to see)':['みた', 'saw'], 'ねます (to sleep or go to bed)':['ねた', 'slept or went to bed'], 'おります (to get off)':['おりた', 'got off'], 'かります (to borrow )':['かりた', 'borrowed'], 'しつれんします (to be brokenhearted)':['しつれんした', 'was brokenhearted'], 'スポーツを します (to play sports)':['スポーツを した', 'played sports'], 'あらいます (to wash)':['あらった', 'washed'], 'そうじを します (to clean)':['そうじを した', 'cleaned']};
 
 
 var allQuestions = [];
@@ -34,18 +34,18 @@ var questions = [];
 
 var item;
 
-function fillArray(dict, array) {
+function fillArray (dict, array) {
   while (array.length > 0) {
-    array.pop();
+    array.pop ();
   }
 
   for (item in dict) {
-      array.push(item);
+      array.push (item);
   }
 }
 
-fillArray(hiraganaDict, allQuestions);
-fillArray(hiraganaDict, questions);
+fillArray (hiraganaDict, allQuestions);
+fillArray (hiraganaDict, questions);
 
 
 var kana1 = {'っか':'kか', 'っき':'kき', 'っく':'kく', 'っけ':'kけ', 'っこ':'kこ', 'っが':'gが', 'っぎ':'gぎ', 'っぐ':'gぐ', 'っげ':'gげ', 'っご':'gご', 'っさ':'sさ', 'っし':'sし', 'っす':'sす', 'っせ':'sせ', 'っそ':'sそ', 'っざ':'zざ','っじ':'jじ', 'っず':'zず', 'っぜ':'zぜ', 'っぞ':'zぞ', 'った':'tた', 'っち':'cち', 'っつ':'tつ', 'って':'tて', 'っと':'tと', 'っだ':'dだ', 'っで':'dで', 'っど':'dど', 'っは':'hは', 'っひ':'hひ', 'っふ':'fふ', 'っへ':'hへ', 'っほ':'hほ', 'っば':'bば', 'っび':'bび', 'っぶ':'bぶ', 'っべ':'bべ', 'っぼ':'bぼ', 'っぱ':'pぱ', 'っぴ':'pぴ', 'っぷ':'pぷ', 'っぺ':'pぺ', 'っぽ':'pぽ', 'っま':'mま', 'っみ':'mみ', 'っむ':'mむ', 'っめ':'mめ', 'っも':'mも', 'っや':'yや', 'っゆ':'yゆ', 'っよ':'yよ', 'っら':'rら', 'っり':'rり', 'っる':'rる', 'っれ':'rれ', 'っろ':'rろ', 'っわ':'wわ', 'ッカ':'kカ', 'ッキ':'kキ', 'ック':'kク', 'ッケ':'kケ', 'ッコ':'kコ', 'ッガ':'gガ', 'ッギ':'gギ', 'ッグ':'gグ', 'ッゲ':'gゲ', 'ッゴ':'gゴ', 'ッセ':'sセ', 'ッシ':'sシ', 'ッス':'sス', 'ッセ':'sセ', 'ッソ':'sソ', 'ッザ':'zザ','ッジ':'jジ', 'ッズ':'zズ', 'ッゼ':'zゼ', 'ッゾ':'zゾ', 'ッタ':'tタ', 'ッチ':'cチ', 'ッツ':'tツ', 'ッテ':'tテ', 'ット':'tト', 'ッダ':'dダ', 'ッデ':'dデ', 'ッド':'dド', 'ッハ':'hハ', 'ッヒ':'hヒ', 'ッフ':'fフ', 'ッヘ':'hヘ', 'ッホ':'hホ', 'ッバ':'bバ', 'ッビ':'bビ', 'ッブ':'bブ', 'ッベ':'bベ', 'ッボ':'bボ', 'ッパ':'pパ', 'ッピ':'pピ', 'ップ':'pプ', 'ッペ':'pペ', 'ッポ':'pポ', 'ッマ':'mマ', 'ッミ':'mミ', 'ッム':'mム', 'ッメ':'mメ', 'ッモ':'mモ', 'ッヤ':'yヤ', 'ッユ':'yユ', 'ッヨ':'yヨ', 'ッラ':'rラ', 'ッリ':'rリ', 'ッル':'rル', 'ッレ':'rレ', 'ッロ':'rロ', 'ッワ':'wワ'};
@@ -54,19 +54,19 @@ var kana3 = {'あ':'a', 'い':'i', 'う':'u','え':'e', 'お':'o', 'か':'ka', '
 var kana4 = {'ん':'n', 'っ':'', 'ン':'n', 'ッ':''};
 
 
-function toRomaji(kana) {
+function toRomaji (kana) {
  var translation = kana;
  for (word in kana1) {
-  translation = translation.replace(word, kana1[word])
+  translation = translation.replace (word, kana1[word])
  }
  for (word in kana2) {
-  translation = translation.replace(word, kana2[word])
+  translation = translation.replace (word, kana2[word])
  }
  for (word in kana3) {
-  translation = translation.replace(word, kana3[word])
+  translation = translation.replace (word, kana3[word])
  }
  for (word in kana4) {
-  translation = translation.replace(word, kana4[word])
+  translation = translation.replace (word, kana4[word])
  }
  return translation;
 }
@@ -74,10 +74,10 @@ function toRomaji(kana) {
 
 var dicts = [dict1, dict2, dict3, dict4, dict5, dict6, dict7, dict8, dict9, dict10, dict11, dict12, dict13, dict14, dict15, dict16, dict17, dict18, dict19];
 
-function toKana(romaji) {
+function toKana (romaji) {
   for (var i = 0; i < dicts.length; i++) {
     for (d in dicts[i]) {
-        if (toRomaji(d) == romaji) return d;
+        if (toRomaji (d) == romaji) return d;
     }
   }
   return romaji; 
@@ -89,19 +89,19 @@ var hira2 = {'shi':'し','chi':'ち', 'tsu':'つ', 'kya':'きゃ', 'kyu':'きゅ
 var hira3 = {'ka':'か', 'ki':'き', 'ku':'く', 'ke':'け', 'ko':'こ', 'ga':'が', 'gi':'ぎ', 'gu':'ぐ', 'ge':'げ', 'go':'ご', 'sa':'さ', 'su':'す', 'se':'せ', 'so':'そ', 'za':'ざ', 'ji':'じ', 'zu':'ず', 'ze':'ぜ', 'zo':'ぞ', 'ta':'た', 'te':'て', 'to':'と', 'da':'だ', 'de':'で', 'do':'ど', 'na':'な', 'ni':'に', 'nu':'ぬ', 'ne':'ね', 'no':'の', 'ha':'は', 'hi':'ひ', 'fu':'ふ', 'he':'へ', 'ho':'ほ', 'ba':'ば', 'bi':'び', 'bu':'ぶ', 'be':'べ', 'bo':'ぼ', 'pa':'ぱ', 'pi':'ぴ', 'pu':'ぷ', 'pe':'ぺ', 'po':'ぽ', 'ma':'ま', 'mi':'み', 'mu':'む', 'me':'め', 'mo':'も', 'ya':'や', 'yu':'ゆ', 'yo':'よ', 'ra':'ら', 'ri':'り', 'ru':'る', 're':'れ', 'ro':'ろ', 'wa':'わ', 'wo':'を'};
 var hira4 = {'a':'あ', 'i':'い', 'u':'う', 'e':'え', 'o':'お', 'n ':'ん', 'nk':'んk', 'ng':'んg', 'ns':'んs', 'nz':'んz', 'nj':'んj', 'nt':'んt', 'nd':'んd', 'nc':'んc', 'nh':'んh', 'nf':'んf', 'nb':'んb', 'np':'んp', 'nm':'んm', 'nr':'んr', 'nw':'んw'};
 
-function toHiragana(romaji) {
+function toHiragana (romaji) {
  var translation = romaji;
  for (word in hira1) {
-  translation = translation.replace(word, hira1[word])
+  translation = translation.replace (word, hira1[word])
  }
  for (word in hira2) {
-  translation = translation.replace(word, hira2[word])
+  translation = translation.replace (word, hira2[word])
  }
  for (word in hira3) {
-  translation = translation.replace(word, hira3[word])
+  translation = translation.replace (word, hira3[word])
  }
  for (word in hira4) {
-  translation = translation.replace(word, hira4[word])
+  translation = translation.replace (word, hira4[word])
  }
  return translation;
 }
