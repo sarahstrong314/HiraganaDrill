f4258c8e9cb1d34b62f896b5cb6ef4d97eac42b1
Sarah Strong <sa967st@gmail.com>
2017-05-13 19:53:56 -0400

    Added English-To-Japanese Option

--- drill.js
+++ drill.js
@@ -5,6 +5,16 @@ function setView() {
   var view = document.getElementById('ViewMenu').selectedIndex;
   if (view == 0) document.getElementById('Question').innerHTML = toKana(document.getElementById('Question').innerHTML);
   else document.getElementById('Question').innerHTML = toRomaji(document.getElementById('Question').innerHTML);
+  
+  if (document.getElementById('ViewMenu').selectedIndex == 0) {
+    for (var i = 0; i < options.length; i++) {
+     options[i].innerHTML = toKana(options[i].innerHTML);
+    }
+  } else {    
+    for (var i = 0; i < options.length; i++) {
+     options[i].innerHTML = toRomaji(options[i].innerHTML);
+    }
+  }
 }
 
 var lesson = 0;
@@ -15,38 +25,39 @@ function setLesson() {
   numLeft = Object.keys(allDicts[lesson]).length;
   fillArray(allDicts[lesson], questions);
   fillArray(allDicts[lesson], allQuestions);
+
+  var lang = document.getElementById('LangMenu').selectedIndex;
  
   document.getElementById('ViewMenu').style.display = 'inline-block';
   document.getElementById('Form').style.display = 'none';
   document.getElementById('Translation').style.display = 'none';
   document.getElementById('GiveUp').style.display = 'none';
-  //changeBreaks(0);
   hideOptions(false);
   
   if (lesson < 3) {
     document.getElementById('ViewMenu').selectedIndex = 0;
     document.getElementById('Romaji').disabled = true;
-    document.getElementById('Instructions').innerHTML = 'Select the correct pronunciation of the character below.';
+    if (lang == 0) document.getElementById('Instructions').innerHTML = 'Select the correct pronunciation of this character.';
+    else document.getElementById('Instructions').innerHTML = 'Select the correct character.';
   } else if (lesson < 8) {
     document.getElementById('ViewMenu').selectedIndex = 1;
     document.getElementById('Romaji').disabled = false;
-    document.getElementById('Instructions').innerHTML = 'Select the correct translation of the word below.';
+    document.getElementById('Instructions').innerHTML = 'Select the correct translation of this word.';
   } else if (lesson == 17 || lesson == 21 || lesson == 23 || lesson == 25) {
     document.getElementById('ViewMenu').style.display = 'none';
     document.getElementById('Form').style.display = 'initial';
     document.getElementById('Translation').style.display = 'inline-block';
     document.getElementById('GiveUp').style.display = 'initial';
     document.getElementById('Input').value = '';
-    //changeBreaks(1);
     hideOptions(true);
-    if (lesson == 17) document.getElementById('Instructions').innerHTML = 'Type the correct te form of the verb below.';
-    else if (lesson == 21) document.getElementById('Instructions').innerHTML = 'Type the correct nai form of the verb below.';
-    else if (lesson == 23) document.getElementById('Instructions').innerHTML = 'Type the correct dictionary form of the verb below.';
-    else document.getElementById('Instructions').innerHTML = 'Type the correct ta form of the verb below.';
+    if (lesson == 17) document.getElementById('Instructions').innerHTML = 'Type the correct te form of the verb.';
+    else if (lesson == 21) document.getElementById('Instructions').innerHTML = 'Type the correct nai form of the verb.';
+    else if (lesson == 23) document.getElementById('Instructions').innerHTML = 'Type the correct dictionary form of the verb.';
+    else document.getElementById('Instructions').innerHTML = 'Type the correct ta form of the verb.';
   } else {
     document.getElementById('ViewMenu').selectedIndex = 0;
     document.getElementById('Romaji').disabled = false;
-    document.getElementById('Instructions').innerHTML = 'Select the correct translation of the word below.';
+    document.getElementById('Instructions').innerHTML = 'Select the correct translation of this word.';
   }
 
   setView();
@@ -77,17 +88,7 @@ function hideOptions(bool) {
   }
 }
 
-/* function changeBreaks(mode) {
-  if (mode == 0) {
-    //
-  } else {
-    //
-  }
-}*/
-
-
 var lastOne = false;
-var answer;
 
 function pickQuestion() {
   stopScore = false;
@@ -101,11 +102,24 @@ function pickQuestion() {
   }
 
   if (questions.length == 1) lastOne = true;
+
   var n = Math.floor(Math.random() * questions.length);
+
+  var newQuestion;
+  var newAnswer;
+
+  if (document.getElementById('LangMenu').selectedIndex == 0) {
+    newQuestion = questions[n];
+    newAnswer = allDicts[lesson][questions[n]];
+  }
+  else {
+    newAnswer = questions[n];
+    newQuestion = allDicts[lesson][questions[n]];
+  }
   
-  if (document.getElementById('ViewMenu').selectedIndex == 0) document.getElementById('Question').innerHTML = questions[n];
-  else if (lesson == 17 || lesson == 21 || lesson == 23 || lesson == 25) document.getElementById('Question').innerHTML = questions[n];
-  else document.getElementById('Question').innerHTML = toRomaji(questions[n]);
+  if (document.getElementById('ViewMenu').selectedIndex == 0) document.getElementById('Question').innerHTML = newQuestion;
+  else if (lesson == 17 || lesson == 21 || lesson == 23 || lesson == 25) document.getElementById('Question').innerHTML = newQuestion;
+  else document.getElementById('Question').innerHTML = toRomaji(newQuestion);
   
   if (lesson == 17 || lesson == 21 || lesson == 23 || lesson == 25) {
     document.getElementById('GiveUp').disabled = false;
@@ -113,8 +127,7 @@ function pickQuestion() {
     document.getElementById('Input').value = '';
     document.getElementById('Input').style.color = 'black';
     document.getElementById('Translation').innerHTML = '(' + allDicts[lesson][questions[n]][1] + ')';
-    answer = allDicts[17][questions[n]][0];
-  } else displayAnswers(questions[n]);
+  } else displayAnswers(newAnswer);
   questions.splice(n, 1);
   document.getElementById('AmIRight').innerHTML = '';
 }
@@ -122,16 +135,31 @@ function pickQuestion() {
 function displayAnswers(answer) {
   var dictionaryLength = Object.keys(allDicts[lesson]).length;
 
+  var randomAnswer;
+  var randomAnswer2;
+
   // picks two different random indices that don't match answer's index
+  
+  if (document.getElementById('LangMenu').selectedIndex == 0) {
+    randomAnswer = allDicts[lesson][allQuestions[Math.floor(Math.random() * dictionaryLength)]];
+    randomAnswer2 = allDicts[lesson][allQuestions[Math.floor(Math.random() * dictionaryLength)]];
 
-  var randomAnswer = allQuestions[Math.floor(Math.random() * dictionaryLength)];
-  var randomAnswer2 = allQuestions[Math.floor(Math.random() * dictionaryLength)];
+    while (answer == randomAnswer)
+      randomAnswer = allDicts[lesson][allQuestions[Math.floor(Math.random() * dictionaryLength)]];
 
-  while (answer == randomAnswer)
+    while (randomAnswer2 == answer || randomAnswer2 == randomAnswer)
+      randomAnswer2 = allDicts[lesson][allQuestions[Math.floor(Math.random() * dictionaryLength)]];
+  
+  } else {    
     randomAnswer = allQuestions[Math.floor(Math.random() * dictionaryLength)];
-
-  while (randomAnswer2 == answer || randomAnswer2 == randomAnswer)
     randomAnswer2 = allQuestions[Math.floor(Math.random() * dictionaryLength)];
+
+    while (answer == randomAnswer)
+      randomAnswer = allQuestions[Math.floor(Math.random() * dictionaryLength)];
+
+    while (randomAnswer2 == answer || randomAnswer2 == randomAnswer)
+      randomAnswer2 = allQuestions[Math.floor(Math.random() * dictionaryLength)];
+  }
 
   // puts the answers in order based on index
 
@@ -141,8 +169,14 @@ function displayAnswers(answer) {
 
   // displays the answers, in order, on the option buttons
 
-  for (var i = 0; i < options.length; i++) {
-    options[i].innerHTML = allDicts[lesson][answers[i]];
+  if (document.getElementById('ViewMenu').selectedIndex == 0) {
+    for (var i = 0; i < options.length; i++) {
+      options[i].innerHTML = answers[i];
+    }
+  } else {
+    for (var i = 0; i < options.length; i++) {
+      options[i].innerHTML = toRomaji(answers[i]);
+    }
   }
 
   // once an option is clicked, displays whether it is correct or incorrect
